cfae5f4ef91f46c79ddda8bf56e2ae732a91144b
progradelmar <113079758+progradelmar@users.noreply.github.com>
2022-10-12 10:11:55 -0600

    updated

--- main.ts
+++ main.ts
@@ -1,1 +1,91 @@
-
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Pickles = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . 2 . . . . . . . . . . . . . 
+        . . 2 2 2 . . . . . . . . . . . 
+        . . 2 2 2 2 2 2 2 2 2 2 2 . . . 
+        . . 2 2 2 2 2 . . . . . . . . . 
+        . . 2 2 2 2 2 2 2 . . . . . . . 
+        . . 2 . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpaceBurger, 200, 0)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.fire, 200)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    scene.cameraShake(4, 500)
+    info.changeLifeBy(-1)
+})
+let Pickles: Sprite = null
+let SpaceBurger: Sprite = null
+SpaceBurger = sprites.create(img`
+    ...........ccccc66666...........
+    ........ccc4444444444666........
+    ......cc444444444bb4444466......
+    .....cb4444bb4444b5b444444b.....
+    ....eb4444b5b44444b44444444b....
+    ...ebb44444b4444444444b444446...
+    ..eb6bb444444444bb444b5b444446..
+    ..e6bb5b44444444b5b444b44bb44e..
+    .e66b4b4444444444b4444444b5b44e.
+    .e6bb444444444444444444444bb44e.
+    eb66b44444bb444444444444444444be
+    eb66bb444b5b44444444bb44444444be
+    fb666b444bb444444444b5b4444444bf
+    fcb666b44444444444444bb444444bcf
+    .fbb6666b44444444444444444444bf.
+    .efbb66666bb4444444444444444bfe.
+    .86fcbb66666bbb44444444444bcc688
+    8772effcbbbbbbbbbbbbbbbbcfc22778
+    87722222cccccccccccccccc22226678
+    f866622222222222222222222276686f
+    fef866677766667777776667777fffef
+    fbff877768f86777777666776fffffbf
+    fbeffeefffeff7766688effeeeefeb6f
+    f6bfffeffeeeeeeeeeeeeefeeeeebb6e
+    f66ddfffffeeeffeffeeeeeffeedb46e
+    .c66ddd4effffffeeeeeffff4ddb46e.
+    .fc6b4dddddddddddddddddddb444ee.
+    ..ff6bb444444444444444444444ee..
+    ....ffbbbb4444444444444444ee....
+    ......ffebbbbbb44444444eee......
+    .........fffffffcccccee.........
+    ................................
+    `, SpriteKind.Player)
+controller.moveSprite(SpaceBurger, 200, 200)
+SpaceBurger.setStayInScreen(true)
+info.setLife(3)
+game.onUpdateInterval(1000, function () {
+    Pickles = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 7 7 7 7 . . . . . . 
+        . . 7 7 7 7 7 7 7 7 7 7 7 . . . 
+        . . 7 7 7 7 7 7 7 7 7 1 7 7 . . 
+        . 7 7 7 7 7 7 7 7 7 1 1 7 7 7 . 
+        . 7 7 7 7 7 1 1 7 7 7 7 7 7 7 7 
+        . 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+        . 7 7 7 7 7 7 7 1 1 1 7 7 7 7 7 
+        . 7 7 7 7 1 1 7 7 7 7 7 7 7 7 7 
+        . 7 7 7 7 7 7 7 7 7 7 1 1 1 7 . 
+        . 7 7 7 7 7 7 7 7 7 7 7 7 7 7 . 
+        . 7 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+        . 7 7 7 7 1 1 1 1 7 7 7 7 . . . 
+        . . 7 7 7 7 7 7 7 7 7 7 7 . . . 
+        . . . . 7 7 7 7 7 7 7 7 . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    Pickles.setVelocity(-100, 0)
+    Pickles.setPosition(160, randint(5, 115))
+    Pickles.setFlag(SpriteFlag.AutoDestroy, true)
+})
